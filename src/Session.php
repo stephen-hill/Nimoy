89b1634f30cd07f10cf1067ba1d2627102a8a6d9
Stephen Hill <stephen@gatekiller.co.uk>
2014-02-12 16:18:51 +0000

    Added the private method generateKey() to generate a pseudo-random hash with hopefully enough entropy.

--- src/Session.php
+++ src/Session.php
@@ -4,9 +4,41 @@ namespace Nimoy
 {
 	class Session extends \Pimple
 	{
+		private $key;
+		
+		/**
+		 * Creates an instance of this class using either an existing session key
+		 * or allowing the constructor to generate a new one.
+		 * @param string $key (Optional)
+		 */
 		public function __construct($key = null)
 		{
-
+			if ($key === null)
+			{
+				$this->key = $this->generateKey();
+			}
+			else
+			{
+				$this->key = $key;
+			}
+		}
+		
+		private function generateKey()
+		{
+			$entropy = array();
+			$entropy[] = hash('sha256', mt_rand());
+			$entropy[] = hash('sha256', uniqid('', true));
+			$entropy[] = hash('sha256', microtime());
+			$entropy[] = hash('sha256', memory_get_usage(true));
+			$entropy[] = hash('sha256', getmypid());
+			$entropy[] = hash('sha256', memory_get_peak_usage(true));
+			$entropy[] = hash('sha256', json_encode(get_defined_constants()));
+			$entropy[] = hash('sha256', json_encode(get_included_files()));
+			$entropy[] = hash('sha256', json_encode(get_loaded_extensions()));
+			$entropy[] = hash('sha256', json_encode(ini_get_all()));
+			$entropy[] = hash('sha256', json_encode(posix_times()));
+			
+			return hash('sha256', implode($entropy));
 		}
 	}
 }
