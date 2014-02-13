155c5b2b3ac06c640aa24d2fce18fb6ed880db0c
Stephen Hill <stephen@gatekiller.co.uk>
2014-02-13 21:33:07 +0000

    Changed include because this file was moved.

--- phpunit.php
+++ phpunit.php
@@ -1,5 +1,5 @@
 <?php
 
 $_SESSION = array();
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 date_default_timezone_set("UTC");
