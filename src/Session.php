f4b88a2746cc8efe613bc9f07dbfcfb97bfb8b61
Stephen Hill <stephen@gatekiller.co.uk>
2014-02-18 16:54:41 +0000

    Converted tabs indentation to 4 spaces.

--- src/Session.php
+++ src/Session.php
@@ -2,77 +2,77 @@
 
 namespace Nimoy
 {
-	use InvalidArgumentException;
-	
-	class Session extends \Pimple
-	{
-		private $key;
-		private $name = '';
-		
-		/**
-		 * Creates an instance of this class using either an existing session key
-		 * or allowing the constructor to generate a new one.
-		 * @param string $key (Optional)
-		 */
-		public function __construct($key = null)
-		{
-			if ($key === null)
-			{
-				$this->key = $this->generateKey();
-			}
-			else
-			{
-				if (strlen($key) < 64)
-				{
-					throw new InvalidArgumentException('Argument $key must contain 64 or more characters.');
-				}
-				
-				$this->key = $key;
-			}
-		}
+    use InvalidArgumentException;
+    
+    class Session extends \Pimple
+    {
+        private $key;
+        private $name = '';
+        
+        /**
+         * Creates an instance of this class using either an existing session key
+         * or allowing the constructor to generate a new one.
+         * @param string $key (Optional)
+         */
+        public function __construct($key = null)
+        {
+            if ($key === null)
+            {
+                $this->key = $this->generateKey();
+            }
+            else
+            {
+                if (strlen($key) < 64)
+                {
+                    throw new InvalidArgumentException('Argument $key must contain 64 or more characters.');
+                }
+                
+                $this->key = $key;
+            }
+        }
 
-		public function getKey()
-		{
-			return $this->key;
-		}
+        public function getKey()
+        {
+            return $this->key;
+        }
 
-		public function getName()
-		{
+        public function getName()
+        {
 
-			return $this->name;
-		}
+            return $this->name;
+        }
 
-		public function setName($name)
-		{
-			if (is_string($name) === false)
-			{
-				throw new InvalidArgumentException('Argument $name must be a string.');
-			}
+        public function setName($name)
+        {
+            if (is_string($name) === false)
+            {
+                throw new InvalidArgumentException('Argument $name must be a string.');
+            }
 
-			$this->name = $name;
-		}
+            $this->name = $name;
+        }
 
-		public function regenerate()
-		{
-			return $this->key = $this->generateKey();
-		}
-		
-		private function generateKey()
-		{
-			$entropy = array();
-			$entropy[] = hash('sha256', mt_rand());
-			$entropy[] = hash('sha256', uniqid('', true));
-			$entropy[] = hash('sha256', microtime());
-			$entropy[] = hash('sha256', memory_get_usage(true));
-			$entropy[] = hash('sha256', getmypid());
-			$entropy[] = hash('sha256', memory_get_peak_usage(true));
-			$entropy[] = hash('sha256', json_encode(get_defined_constants()));
-			$entropy[] = hash('sha256', json_encode(get_included_files()));
-			$entropy[] = hash('sha256', json_encode(get_loaded_extensions()));
-			$entropy[] = hash('sha256', json_encode(ini_get_all()));
-			$entropy[] = hash('sha256', json_encode(posix_times()));
-			
-			return hash('sha256', implode($entropy));
-		}
-	}
+        public function regenerate()
+        {
+            return $this->key = $this->generateKey();
+        }
+        
+        private function generateKey()
+        {
+            $entropy = array();
+            $entropy[] = hash('sha256', mt_rand());
+            $entropy[] = hash('sha256', uniqid('', true));
+            $entropy[] = hash('sha256', microtime());
+            $entropy[] = hash('sha256', memory_get_usage(true));
+            $entropy[] = hash('sha256', getmypid());
+            $entropy[] = hash('sha256', memory_get_peak_usage(true));
+            $entropy[] = hash('sha256', json_encode(get_defined_constants()));
+            $entropy[] = hash('sha256', json_encode(get_included_files()));
+            $entropy[] = hash('sha256', json_encode(get_loaded_extensions()));
+            $entropy[] = hash('sha256', json_encode(ini_get_all()));
+            $entropy[] = hash('sha256', json_encode(posix_times()));
+            
+            return hash('sha256', implode($entropy));
+        }
+    }
 }
--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -5,42 +5,42 @@ use \Pimple;
 
 class SessionTest extends PHPUnit_Framework_TestCase
 {
-	public function testConstructor()
-	{
-		$s = new Session();
-		$this->assertInstanceOf(Session::class, $s);
-		$this->assertInstanceOf(Pimple::class, $s);
-	}
+    public function testConstructor()
+    {
+        $s = new Session();
+        $this->assertInstanceOf(Session::class, $s);
+        $this->assertInstanceOf(Pimple::class, $s);
+    }
 
-	public function testValidKey()
-	{
-		$key = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4a';
-		$s = new Session($key);
-		$this->assertEquals($key, $s->getKey());
-	}
+    public function testValidKey()
+    {
+        $key = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4a';
+        $s = new Session($key);
+        $this->assertEquals($key, $s->getKey());
+    }
 
-	/**
-	 * @expectedException InvalidArgumentException
-	 */
-	public function testInvalidKey()
-	{
-		$key = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4';
-		$s = new Session($key);
-	}
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidKey()
+    {
+        $key = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4';
+        $s = new Session($key);
+    }
 
-	public function testRegenerate()
-	{
-		$s = new Session();
-		$a = $s->getKey();
-		$b = $s->regenerate();
+    public function testRegenerate()
+    {
+        $s = new Session();
+        $a = $s->getKey();
+        $b = $s->regenerate();
 
-		$this->assertNotEquals($a, $b);
-	}
+        $this->assertNotEquals($a, $b);
+    }
 
-	public function testGetSetName()
-	{
-		$s = new Session();
-		$s->setName('Session Name');
-		$this->assertEquals('Session Name', $s->getName());
-	}
+    public function testGetSetName()
+    {
+        $s = new Session();
+        $s->setName('Session Name');
+        $this->assertEquals('Session Name', $s->getName());
+    }
 }
