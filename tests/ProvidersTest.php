c96b0dbf81ec0699ab0e8447a82ff8f46e316f8f
Stephen Hill <stephen@gatekiller.co.uk>
2014-02-26 12:28:55 +0000

    Changed structure so that the test can run the same assertation against different providers. Also now tests different types and objects.

--- tests/ProvidersTest.php
+++ tests/ProvidersTest.php
@@ -2,35 +2,58 @@
 
 use Nimoy\MemcachedProvider;
 
+/**
+ * @todo Implement a test for invalid keys
+ */
 class MemcachedProviderTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @group MemcachedProvider
-	 */
-	public function testSetGetDelete()
+	private $providers = array();
+
+	public function setUp()
 	{
-		$m = new MemcachedProvider();
-		$m->set('a62f2', 'f32b6');
-		$v = $m->get('a62f2');
-
-		$this->assertSame('f32b6', $v);
-
-		$d = $m->delete('a62f2');
-		$v = $m->get('a62f2');
-
-		$this->assertTrue($d);
-		$this->assertFalse($v);
+		$this->providers['Memcached'] = new MemcachedProvider();
 	}
 
 	/**
-	 * @group MemcachedProvider
+	 * @group Providers
 	 */
-	public function testExpires()
+	public function testSetGetTypes()
 	{
-		$m = new MemcachedProvider();
-		$this->assertTrue
-		(
-			$m->set('a62f2', 'f32b6', 31556926)
-		);
+		foreach ($this->providers as $n => $p)
+		{
+			// Test String
+			$p->set('StringKey', 'Hello World');
+			$this->assertSame('Hello World', $p->get('StringKey'));
+
+			// Test Integers
+			$p->set('IntegerKey', 1234);
+			$this->assertSame(1234, $p->get('IntegerKey'));
+
+			// Test stdClass
+			$p->set('stdClassKey', new stdClass());
+			$this->assertEquals(new stdClass(), $p->get('stdClassKey'));
+
+			// Test Array
+			$p->set('arrayKey', ['key' => 'value']);
+			$this->assertSame(['key' => 'value'], $p->get('arrayKey'));
+
+			// Test Float
+			$p->set('floatKey', 1.10);
+			$this->assertSame(1.10, $p->get('floatKey'));
+
+			// Test Binary
+			$p->set('binaryKey', 0b001001101);
+			$this->assertSame(0b001001101, $p->get('binaryKey'));
+
+			// Test Delete
+			$p->set('DeleteMe', 'Please Delete This.');
+			$this->assertTrue($p->delete('DeleteMe'));
+
+			// Test long expiry time
+			$this->assertTrue($p->set('key', 'value', 31556926));
+
+			// Test short expiry time
+			$this->assertTrue($p->set('key', 'value', 1));
+		}
 	}
 }
